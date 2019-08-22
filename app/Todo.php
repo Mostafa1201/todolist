4d0168a68c2e979c2018fff09d5ff0e8c6bd090b
mostafa1201 <mostafa1201@hotmail.com>
2019-08-22 11:17:44 +0200

    making the code cleaner

--- app/Todo.php
+++ app/Todo.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    /**
+     * Get the user that owns the todo.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
